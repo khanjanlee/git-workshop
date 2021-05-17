7ca971487f38fd590a32947d5fd0e34cb1f855ba
Khanjan nath <khanjan@leewayhertz.com>
2021-05-17 12:14:02 +0530

    First Commit

---
+++ github.sh
@@ -0,0 +1,8 @@
+git init
+git add *
+git commit -m "First Commit"
+git config user.name $MyName
+sit config user.mail $MyEmail
+git branch -M main
+git remote add origin https://github.com/khanjanlee/git-workshop.git
+git push -u origin main
