011c459e53bdedb665bae44644bda42a74ce649a
Khanjan nath <khanjan@leewayhertz.com>
2021-05-17 12:18:34 +0530

    commiting the github.sh file

--- github.sh
+++ github.sh
@@ -6,3 +6,11 @@ sit config user.mail $MyEmail
 git branch -M main
 git remote add origin https://github.com/khanjanlee/git-workshop.git
 git push -u origin main
+
+DID_SOME_CHANGES
+
+git checkout -b developement
+git add *
+git commit -m "commit to developement branch"
+git push  --set-upstream origin developement
+git push 
